d8e5c7649c7d136ffa6cde25aeff6e26211252d0
Fernando Soares <fndcaique@gmail.com>
2022-08-25 11:46:04 -0300

    initial commit

---
+++ src/main/java/com/fndcaique/fndstoreapispring/FndStoreApiSpringApplication.java
@@ -0,0 +1,12 @@
+package com.fndcaique.fndstoreapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FndStoreApiSpringApplication {
+  public static void main(String[] args) {
+
+    SpringApplication.run(FndStoreApiSpringApplication.class, args);
+  }
+}
---
+++ src/test/java/com/fndcaique/fndstoreapispring/FndStoreApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fndcaique.fndstoreapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FndStoreApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
