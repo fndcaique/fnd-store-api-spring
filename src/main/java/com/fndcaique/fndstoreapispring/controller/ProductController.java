1adf39ff05929b93208b6ebdb24a264e917a540d
Fernando Soares <fndcaique@gmail.com>
2022-08-30 23:54:36 -0300

    add product create request validation

--- src/main/java/com/fndcaique/fndstoreapispring/controller/ProductController.java
+++ src/main/java/com/fndcaique/fndstoreapispring/controller/ProductController.java
@@ -1,10 +1,17 @@
 package com.fndcaique.fndstoreapispring.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fndcaique.fndstoreapispring.dto.ProductDTO;
@@ -45,7 +53,7 @@ public class ProductController {
   }
 
   @PostMapping
-  public ResponseEntity<Product> create(@RequestBody(required = true) ProductDTO productDTO) {
+  public ResponseEntity<Product> create(@RequestBody(required = true) @Valid ProductDTO productDTO) {
     return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(productDTO.build()));
   }
 
@@ -67,6 +75,18 @@ public class ProductController {
     }
     repository.deleteById(id);
     return ResponseEntity.ok().build();
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return errors;
   }
 
   // todo
--- src/main/java/com/fndcaique/fndstoreapispring/dto/ProductDTO.java
+++ src/main/java/com/fndcaique/fndstoreapispring/dto/ProductDTO.java
@@ -1,5 +1,10 @@
 package com.fndcaique.fndstoreapispring.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import com.fndcaique.fndstoreapispring.model.Product;
 
 import lombok.AllArgsConstructor;
@@ -10,7 +15,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
+  @NotBlank
+  @Pattern(regexp = "[a-zA-Z]+.*")
   private String name;
+
+  @NotNull(message = "must not be null and must be a number")
+  @Min(value = 0)
   private Integer quantity;
 
   public Product build() {
